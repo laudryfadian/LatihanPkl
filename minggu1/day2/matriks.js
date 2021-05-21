bcf045da31d22688e305421e8d7bf68ad98c655e
LAUDRY IZA FADIAN <63417742+laudryfadian@users.noreply.github.com>
2021-05-21 14:51:17 +0700

    Update and rename matriks.html to matriks.js

--- minggu1/day2/matriks.js
+++ minggu1/day2/matriks.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <script>
     var items =[
       [1, 2],
       [3, 4]
@@ -22,7 +12,5 @@
     console.log(items[0][1] + items2[0][1]); 
     console.log(items[1][0] + items2[1][0]); 
     console.log(items[1][1] + items2[1][1]); 
-    </script>
-</body>
-</html>
+
 
